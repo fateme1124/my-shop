cd05f44d23f66226633737f5a49ade154bc269df
fatemeh dadkhah tehrani <dadkhahfateme42@gmail.com>
2022-08-20 20:16:28 +0430

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Switch , Route , Redirect  } from 'react-router-dom';
+
+// component
+import Store from './components/Store';
+import ProductDetails from './components/ProductDetails';
+import Navbar from './components/shared.js/Navbar';
+import ShopCart from './components/ShopCart';
+
+// context
+import ProductsContextProvider from './context/ProductContextProvider';
+import CartContextProvider from './context/CartContextProvider';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ProductsContextProvider>
+      <CartContextProvider>
+        <Navbar />
+        <Switch>
+          <Route path='/products/:id' component={ProductDetails} />
+          <Route path='/products' component={Store} />
+          <Route path='/cart' component={ShopCart} />
+          <Redirect to='/products' />
+        </Switch> 
+      </CartContextProvider>
+    </ProductsContextProvider>
   );
 }
 
